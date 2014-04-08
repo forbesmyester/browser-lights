f81c8af7090484b2dddb932058034c59577fbf52
Matthew Forrester <matt@keyboardwritescode.com>
2014-04-08 08:25:43 +0100

    Fix lights wrapping (now they only ever draw one line)

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 define(function() {
 	var lightsElement = null;
+
+	var getElementWidth = function(element) {
+		var computedStyle = window.getComputedStyle ? getComputedStyle(element, '') : element.currentStyle;
+		return element.clientWidth - (
+			parseInt(computedStyle.paddingLeft) + 
+			parseInt(computedStyle.paddingRight)
+		);
+	};
+
 	return function(color) {
 		var blockWidth = 4;
-		var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 		if (!lightsElement) {
 			lightsElement = document.createElement('div');
 			document.body.insertBefore(lightsElement, document.body.firstChild);
@@ -20,7 +28,10 @@ define(function() {
 			return;
 		}
 		document.body.firstChild.insertBefore(element, document.body.firstChild.firstChild);
-		if (document.body.firstChild.children.length * blockWidth > windowWidth) {
+		while (
+			(document.body.firstChild.children.length) &&
+			(document.body.firstChild.children.length * blockWidth > getElementWidth(lightsElement))
+		) {
 			document.body.firstChild.lastChild.remove();
 		}
 	};
